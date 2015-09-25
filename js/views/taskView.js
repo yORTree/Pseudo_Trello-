3dc4e669f2c9c1d19295c3d1f4e7057587cd5449
Jennifer <yortree@gmail.com>
2015-09-25 15:44:35 -0700

    Clear input field function works

--- js/views/taskView.js
+++ js/views/taskView.js
@@ -6,11 +6,10 @@ console.log("TASK VIEW IS RENDERING");
 	// var greeting= "<h1> Greetings, "+ this.model.get('username') +" !!</h1>";
 	var taskTitle = '<div id="currentTasks"><h2> The task below has been saved! </h2></div>';
 	var taskItem = this.model.get('description');
-	var showAll = '<br><br><button id="showAll">Show All Tasks</button>';
 	var returnBtn = '<br><br><button id="userPage">Return</button>';
     
 	var input = '<br><div id="taskList"><ul><li></li></ul></div>';
-	this.$el.html("<div id='taskview'>"+taskTitle +taskItem + returnBtn + input + showAll+"</div>");
+	this.$el.html("<div id='taskview'>"+taskTitle +taskItem + returnBtn + input +"</div>");
 
 
 	console.log('TASKS HAS BEEN ADDED!',this.el);
@@ -27,7 +26,8 @@ console.log("TASK VIEW IS RENDERING");
   },
 
    returnHome : function(){
-    	$("#taskview").hide();
+    	$("#taskview").empty();
+
     	// $("#login-area").show();
     }
 });

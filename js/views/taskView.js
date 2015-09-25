614fda0b0ec2d99a13108f413825cd9ed03dcc4a
Jennifer <yortree@gmail.com>
2015-09-25 15:59:19 -0700

    fix merge conflicts

--- js/views/taskView.js
+++ js/views/taskView.js
@@ -6,15 +6,11 @@ console.log("TASK VIEW IS RENDERING");
 	// var greeting= "<h1> Greetings, "+ this.model.get('username') +" !!</h1>";
 	var taskTitle = '<div id="currentTasks"><h2> The task below has been saved! </h2></div>';
 	var taskItem = this.model.get('description');
-<<<<<<< HEAD
-	var returnBtn = '<br><br><button id="userPage">Return</button>';
-    
-	var input = '<br><div id="taskList"><ul><li></li></ul></div>';
-=======
-	var returnBtn = '<br><br><button id="logout">Return to Homepage</button>';
+
+	var returnBtn = '<br><br><button id="homePage">Return to Homepage</button>';
 
 	var input = '<br><div id="taskList"></div>';
->>>>>>> master
+
 	this.$el.html("<div id='taskview'>"+taskTitle +taskItem + returnBtn + input +"</div>");
 
 
